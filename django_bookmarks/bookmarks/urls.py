69614a3e1cc542f392411c4810c66579e80be8c6
Todd Hayton <thayton@neekanee.com>
2014-11-05 11:35:52 -0500

    chapter 4 updates

--- django_bookmarks/bookmarks/urls.py
+++ django_bookmarks/bookmarks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^user/(\w+)/$', views.user_page, name='user_page'),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', views.logout_page, name='logout'),
+    url(r'^register/$', views.register_page, name='register'),
 )
--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -5,6 +5,7 @@ from django.template.loader import get_template
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
+from bookmarks.forms import *
 
 def main_page(request):
     return render_to_response('main_page.html', {'user': request.user})
@@ -21,3 +22,19 @@ def user_page(request, username):
 def logout_page(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+                email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect(reverse('home'))
+    else:
+        form = RegistrationForm()
+
+    reqc = RequestContext(request, {'form': form})
+    return render_to_response('registration/register.html', reqc)
